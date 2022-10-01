0cbedf1c25feaa24da06a6a5d9bb215d65ee9402
João Oliveira <jmlcoliveira@gmail.com>
2022-10-01 15:45:29 +0100

    ex4

--- tp1/src/Main.java
+++ tp1/src/Main.java
@@ -1,17 +1,20 @@
 import searchalgorithm.*;
 import undirectedgraph.*;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         Graph graph = Romenia.defineGraph();
-        graph.showLinks();
-        graph.showSets();
         Node n;
-        for(Algorithms a: Algorithms.values()){
+        ArrayList<String> prov = new ArrayList<>();
+        prov.add("Dobrogea");
+        prov.add("Banat");
+        //for(Algorithms a: Algorithms.values()){
             graph.reset();
-            n = graph.searchSolution("Arad", "Bucharest", "Dobrogea",a);
-            System.out.println(a.name());
+            n = graph.searchSolution("Arad", "Bucharest", prov,Algorithms.AStarSearch);
+            //System.out.println(a.name());
             graph.showSolution(n);
-        }
+        //}
     }
 }
